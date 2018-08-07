986b20db0e1a6078b6bcd9ee753658025db33c03
VincentChen04 <1541372584@qq.com>
2018-08-08 00:51:46 +0800

    更新了错误码的处理与依赖密码的设置错误码

--- MathGame/GameCheck.cpp
+++ MathGame/GameCheck.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "GameCheck.h"
-
+#include <vector>
+using namespace std;
 
 GameCheck::GameCheck()
 {
@@ -10,12 +11,33 @@ bool GameCheck::isStop()
 {
 	if (GameCheck::stopCode == 0) {
 		return false;
-	} else return true;
+	}
+	switch (GameCheck::stopCode) {
+	case 1:
+		printf("你所提交的答案错误,你失败了！\n");
+		break;
+	case 2:
+		printf("内部程序出错.\n");
+		break;
+	default:
+		printf("发生了未知错误");
+		;
+	}
+	return true;
 
 }
 
 void GameCheck::setStopcode(int code, int setPass)
 {
+	if (encrypt(setPass) == code) {
+		GameCheck::stopCode = code;
+	}
+}
+
+int GameCheck::encrypt(int pass)
+{
+	int ipass = pass % 2 + 1;
+	return ipass;
 }
 
 
