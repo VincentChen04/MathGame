005fd92c0cc52e407c7fefaa1c5e448c871c03dc
VincentChen04 <1541372584@qq.com>
2018-08-07 20:41:22 +0800

    简易随机数生成。

--- MathGame/RandGen.cpp
+++ MathGame/RandGen.cpp
@@ -1,11 +1,33 @@
 #include "stdafx.h"
 #include "RandGen.h"
-
+#include <time.h>
+#include <stdlib.h>
 
 RandGen::RandGen()
 {
+	srand((unsigned int)(time(NULL)));
+}
+void RandGen::genRandNum(int MODE)
+{
+	RandGen::randNum1 = 10 + rand() % 90;
+	RandGen::randNum2 = 10 + rand() % 90;
+	RandGen::total = getNum1() + getNum2();
 }
 
+int RandGen::getNum1()
+{
+	return RandGen::randNum1;
+}
+
+int RandGen::getNum2()
+{
+	return RandGen::randNum2;
+}
+
+int RandGen::getTotal()
+{
+	return RandGen::total;
+}
 
 RandGen::~RandGen()
 {
--- MathGame/RandGen.h
+++ MathGame/RandGen.h
@@ -3,6 +3,15 @@ class RandGen
 {
 public:
 	RandGen();
+	void genRandNum(int MODE);
+	int getNum1();
+	int getNum2();
+	int getTotal();
 	~RandGen();
+private:
+	int randNum1;
+	int randNum2;
+	int total;
+	const int HELL_MODE=5,EXPERT_MODE = 4,HARD_MODE = 3, MEDIUM_MODE = 2, EASY_MODE = 1, NOOB_MODE = 0;
 };
 
