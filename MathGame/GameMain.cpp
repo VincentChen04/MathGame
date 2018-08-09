76fbcba6d730b80e5ec9e8abbe17bea3d8380964
VincentChen04 <1541372584@qq.com>
2018-08-09 19:07:33 +0800

    按照某人的意见小改了一下并开了新坑

--- MathGame/GameMain.cpp
+++ MathGame/GameMain.cpp
@@ -2,13 +2,24 @@
 #include "GameMain.h"
 #include "RandGen.h"
 #include "GameCheck.h"
+#include "PlaySaver.h"
 RandGen randgen;
 GameCheck checkEngine;
+PlaySaver saves;
 int RandMode;
+
 using namespace std;
+
 GameMain::GameMain()
 {
-	printf("您要选择哪个难度呢？\n1) 菜鸟模式\t极其简单的20以内加减 \n2) 普通模式\t正常人可以承受的难度\n3) 学霸模式\t计算量稍大，学霸请无视\n4) 疯狂模式\t数学狂人的试炼\n5) 专家模式\t颇有众人皆醉我独醒的意境.\n6) Vincent的试炼\t这是非人类题目，慎入！\n 请键入您所要选择难度的序号。");
+	printf("您要选择哪个难度呢？\n");
+	printf("1) 菜鸟模式\t极其简单的20以内加减 \n");
+	printf("2) 普通模式\t正常人可以承受的难度\n");
+	printf("3) 学霸模式\t计算量稍大，学霸请无视\n");
+	printf("4) 疯狂模式\t数学狂人的试炼\n");
+	printf("5) 专家模式\t颇有众人皆醉我独醒的意境.\n");
+	printf("6) Vincent的试炼\t这是非人类题目，慎入！\n ");
+	printf("请键入您所要选择难度的序号。\t");
 	scanf_s("%d", &RandMode);
 	--RandMode;
 	if (RandMode < 0 || RandMode > 5) checkEngine.setStopcode(2, 2);
---
+++ MathGame/PlaySaver.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "PlaySaver.h"
+
+
+PlaySaver::PlaySaver()
+{
+	
+}
+
+
+PlaySaver::~PlaySaver()
+{
+}
